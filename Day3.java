dfc35a38a6bdcd161aeea8f97ba4eb31a905c79b
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-04 10:04:34 +0800

    Menampilkan System.out.println

---
+++ Day3.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Saya Aswad");
+    System.out.println("Alamat Rangas");
+    System.out.println("Kelas B Informatika");
+  }
+}

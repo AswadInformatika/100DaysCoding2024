2d312dac6af709f076b3070debf933d5f131d8f1
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-05 12:24:55 +0800

    menampilkan print dan printf

---
+++ day4.java
@@ -0,0 +1,10 @@
+package coding;
+
+public class Day4 {
+    
+    public static void main(String[] args) {
+        System.out.print("Day 4");
+        System.out.printf("\nNama saya %s \nUmur %d Tahun", "Aswad", 19);
+        
+    }
+}

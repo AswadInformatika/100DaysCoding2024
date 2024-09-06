cb65d3ad20c2184ef52b753c04ce3aa67e08bf5a
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-06 12:08:58 +0800

    menampilkan komentar

---
+++ day5.java
@@ -0,0 +1,17 @@
+package coding;
+//package berupa folder yang dalamnya berisi program java
+public class Day4 {
+    //bagian class
+    public static void main(String[] args) {
+      //method main atau awal program java berjalan (wajib)
+        System.out.print("Day 5");
+        System.out.printf("\nNama saya %s \nUmur %d Tahun", "Aswad", 19);
+        /* statement atau perintah untuk menampilkan teks ke layar monitor
+        print: menampilkan teks tanpa ganti baris
+        prinf: menampilkan teks tapi berisi format kosong yang harus di isi 
+        contoh format nya seperti:
+        %s format untuk teks atau tipe data String
+        \n format pengganti enter atau baris baru
+        %s format yang isinya bilangan seperti 19*/ 
+    }
+}

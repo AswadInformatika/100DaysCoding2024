a395c3c045dc2a7af6081b10cb2cd1034e033c3c
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-07 12:10:08 +0800

    Variabel: Deklarasi

---
+++ day6.java
@@ -0,0 +1,10 @@
+package day6;
+//package berupa folder yang dalamnya berisi program java
+public class Main {
+    //bagian class
+    public static void main(String[] args) {
+        //method main
+        String nama;
+        //membuat variabel kosong bertipe data String
+    }  
+}

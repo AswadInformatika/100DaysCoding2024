9734221bd5152d7c15a3f213c32cd3c650f6dac9
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-08 12:26:27 +0800

    Variabel: Inisialisasi

---
+++ day7.java
@@ -0,0 +1,18 @@
+package day7;
+//package berupa folder yang dalamnya berisi program java
+public class Main {
+    //bagian class
+    public static void main(String[] args) {
+        //method main
+        String nama;
+        nama = "Aswad";
+        //inisialisasi setelah deklarasi atau variabel di isi dulu kemudian nilainya di baris berikutnya
+        System.out.println("Nama " + nama);
+        //menampilkan variabel yang sudah di buat
+        String alamat = "Rangas";
+        //inisialisasi saat deklarasi atau variabel di beri nilai langsung
+        System.out.println("Alamat " + alamat);
+        //menampilkan variabel yang sudah di buat
+        
+    }  
+}

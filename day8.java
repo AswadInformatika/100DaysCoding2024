67af7d4efc02a6ff66f57d8aff340e6bb68681dc
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-09 15:07:23 +0800

    Variabel: Update

---
+++ day8.java
@@ -0,0 +1,25 @@
+package day8;
+//berupa folder yang dalamnya berisi program java
+public class Main {
+    //bagian class
+    public static void main(String[] args) {
+        //method main
+        String nama = "Aswad"; 
+        //inisialisasi variabel setelah deklarasi 
+        System.out.println("Nama " + nama);
+        
+        nama = "Swad";
+        //update nilai variabel
+        System.out.println("Nama " + nama);
+        
+        
+        int umur = 19;
+        //inisialisasi variabel setelah deklarasi
+        System.out.println("Umur " + umur);
+        
+        umur = 15;
+        //update nilai variabel
+        System.out.println("Umur impian " + umur);
+    }
+    
+}

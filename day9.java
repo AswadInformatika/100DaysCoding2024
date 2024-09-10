2b3d6b96a3a0045ed4442148f1121153ed00fc57
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-10 18:12:25 +0800

    Variabel : Constanta (Final Variable)

---
+++ day9.java
@@ -0,0 +1,18 @@
+package day9;
+//package berupa folder yang berisi program java
+public class Main {
+//bagian class   
+    public static void main(String[] args) {
+       //main method
+       
+       final String nama = "Aswad";
+       final String nim = "D0224020";
+       final char kelas = 'B';
+       final String prodi = "Informatika";
+       /*variabel final dimana nilainya tidak dapat di ubah setelah inisialiasi(nilainya di isi)
+       inisialisasi saat deklarasi atau variabel di beri nilai langsung*/
+       
+       System.out.println("Nama: " + nama + " NIM: " + nim + " Kelas: " + kelas + " Prodi: " + prodi);
+       //memampilkan variabel biodata yang sudah di buat dalam 1 baris
+    }
+}

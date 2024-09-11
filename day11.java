500363469b07a0f2cc9d23efc2637bfa98e67f7c
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-12 00:39:11 +0800

    Tipe Data: Short

---
+++ day11.java
@@ -0,0 +1,14 @@
+package day11;
+//package berupa folder yang dalamnya berisi program java
+public class Main {
+//bagian class    
+    public static void main(String[] args) {
+        //main method
+        
+        short jumlah = 1000;
+        //inisialisasi variabel saat deklarasi atau nilai variabel di isi langsung
+        //tipe data short untuk bilangan bulat dari -32768 sampai 32767
+        System.out.println("Jumlah " + jumlah);
+        //menampilkan output variabel yang sudah di buat
+    }  
+}

a543d31919af4b2a12733bd73cf7b1e552d2b389
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-11 09:06:32 +0800

    Tipe data: byte

---
+++ day10.java
@@ -0,0 +1,12 @@
+package day10;
+//package berupa folder yang berisi program.java
+public class Main {
+//bagian class    
+    public static void main(String[] args) {
+        //method main
+        byte banyak = 5;
+        //inisialisasi variabel saat deklarasi atau nilai variabel di isi langsung
+        System.out.println("Banyaknya buku: " + banyak);
+        //menampilkan variabel yang sudah di buat
+    }
+}

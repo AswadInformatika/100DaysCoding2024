78c3e98e5236e17dffba7c1612e1207142dde4a8
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-13 19:32:40 +0800

    Tipe Data: Integer

---
+++ day12.java
@@ -0,0 +1,13 @@
+package day12;
+//package berupa folder yang berisi program java
+public class Main {
+    //bagian class
+    public static void main(String[] args) {
+        //main method
+        int umur = 19;
+        //inisialisasi variabel saat deklarasi atau nilai variabel di isi langsung
+        //tipe data integer untuk bilangan bulat mulai dari -2147483648 sampai 2147483647
+        System.out.println("Umur " + umur);
+        //menampilkan variabel yang sudah di buat
+    }    
+}

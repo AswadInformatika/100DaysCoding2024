ae9f72d1af0cbc906129c26fa23afcdab0baf968
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-14 20:21:59 +0800

    Evaluasi Pertama

--- day13.java
+++ day13.java
@@ -5,39 +5,18 @@ public class Main {
         System.out.printf("Nim: %s", "D0224020");
         System.out.println();
         System.out.printf("Umur %d", 19);
-    }  
-}
 
-
-public class Main {
-    public static void main(String[] args) {
        //deklarasi variabel itu penulisan tipe data tanpa memberikan nilai pada variabelnya
         /*inisialisasi itu pengisian nilai variabel yang sudah dibuat*/             
-    }
-}
-
-
-public class Main {
-    public static void main(String[] args) {
+        
         System.out.printf("Nama: %s", "Aswad");
         System.out.println();
         System.out.printf("Nim: %s", "D0224020");
         System.out.println();
         System.out.printf("Umur %d", 19);
-    }   
-}
-
-
-public class Main {
-    public static void main(String[] args) {
+ 
      final String nama = "Aswad"; 
-    }
-    
-}
-
-
-public class Main {
-    public static void main(String[] args) {
+  
         int a = 5;
         int b = 8;
         int c = a;
@@ -45,12 +24,7 @@ public class Main {
         b = c;
         System.out.println(a);
         System.out.println(b);
-    }  
-}
-
-
-public class Main {
-    public static void main(String[] args) {
+   
         byte minimal = -128;
         byte maximal = 127;
         System.out.println(minimal);

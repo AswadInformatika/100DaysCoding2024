6d887e939a0e1f5caa56acfd4b50df2295b533fa
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-15 09:39:13 +0800

    Tipe Data: Integer

--- day12.java
+++ day12.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //main method
         int umur = 19;
-        //inisialisasi variabel saat deklarasi atau nilai variabel di isi langsung
+        //inisialisasi variabel saat deklarasi
         //tipe data integer untuk bilangan bulat mulai dari -2147483648 sampai 2147483647
         System.out.println("Umur " + umur);
         //menampilkan variabel yang sudah di buat

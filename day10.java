7ecaa40d858457f6803efd8bc9993c0538ff9504
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-15 09:37:06 +0800

    Tipe Data: Byte

--- day10.java
+++ day10.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //method main
         byte banyak = 5;
-        //inisialisasi variabel saat deklarasi atau nilai variabel di isi langsung
+        //inisialisasi variabel setelah deklarasi 
         System.out.println("Banyaknya buku: " + banyak);
         //menampilkan variabel yang sudah di buat
     }

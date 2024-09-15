9837cc232809d7e58052255e8bf0c399a330d28d
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-15 10:16:08 +0800

    Tipe Data: Long

---
+++ day14.java
@@ -0,0 +1,14 @@
+package day14;
+//package berupa folder yang dalamnya berisi program java
+public class Main {
+  //bagian class  
+    public static void main(String[] args) {
+     //main method
+     long panjang = 10000L;
+     //tipe data long untuk menampung angka yang lebih besar
+     //min: 9223372036854775808 / -2⁶³. max: 9223372036854775807 / 2⁶³-1
+     //inisialisasi saat deklarasi
+     System.out.println("Panjangnya " + panjang);
+     //menampilkan nilai variabel
+    }
+}

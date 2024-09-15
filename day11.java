77c3b7716cbadd7d7703dca20d477dac779a3316
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-15 09:35:08 +0800

    Tipe Data: Short

--- day11.java
+++ day11.java
@@ -6,7 +6,7 @@ public class Main {
         //main method
         
         short jumlah = 1000;
-        //inisialisasi variabel saat deklarasi atau nilai variabel di isi langsung
+        //inisialisasi variabel saat deklarasi
         //tipe data short untuk bilangan bulat dari -32768 sampai 32767
         System.out.println("Jumlah " + jumlah);
         //menampilkan output variabel yang sudah di buat

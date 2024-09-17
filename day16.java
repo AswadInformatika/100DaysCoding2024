82fc022cf54f55ad50af9de5423eed00b1575450
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-17 21:05:08 +0800

    Tipe Data: Float

---
+++ day16.java
@@ -0,0 +1,10 @@
+package day16;
+public class Main {
+    public static void main(String[] args) {
+        float koma = 50.5f;
+        //tipe data primitif float untuk menampung angka berkoma
+        //tampungan angkanya lebih sedikit dari double
+        System.out.println("Bilangan berkoma " + koma);
+        //menampilkan nilai variabel
+    }
+}

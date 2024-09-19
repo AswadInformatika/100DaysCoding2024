25352990712e4941a87e58c307bd6189551301b5
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-19 21:50:02 +0800

    Tipe Data: Boolean

--- day18.java
+++ day18.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         boolean belajar = true;
         boolean bohong = false;
+        //tipe data primitif
         //boolean: tipe data yang hanya bernilai true dan false
         System.out.println("Belajar " + belajar);
         System.out.println("Bohong? " + bohong);

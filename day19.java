ac45c59f2334a85cbbacdc270dbf6a7d31660485
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-20 23:16:27 +0800

    Tipe Data Reference: String

---
+++ day19.java
@@ -0,0 +1,10 @@
+package day19;
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Aswad";
+        //tipe data primitif atau reference
+        //yang menampung kata atau sejumlah karakter selagi dalam petik dua " "
+        System.out.println("Hello, World!");
+    }
+}

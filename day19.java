6662611a21d031e79d21524da8dc58a105d03076
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-20 23:28:35 +0800

    Tipe Data Reference: String

--- day19.java
+++ day19.java
@@ -3,7 +3,7 @@ public class Main {
     
     public static void main(String[] args) {
         String nama = "Aswad";
-        //tipe data primitif atau reference
+        //tipe data non primitif atau reference
         //yang menampung kata atau sejumlah karakter selagi dalam petik dua " "
         System.out.println("Nama saya " + nama);
     }

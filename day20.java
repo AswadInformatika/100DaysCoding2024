6ffdd2dc64f19421b955405132b657ad33de0274
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-21 22:27:38 +0800

    Tipe Data Reference: Byte

---
+++ day20.java
@@ -0,0 +1,11 @@
+package day20;
+public class Main {
+    public static void main(String[] args) {
+        Byte b = 2;
+        //tipe data reference atau non primitif
+        //yang membedakan dengan primitif adalah penulisannya
+        //kalau non primitif awal kata menggunakan huruf kapital, primitif semua huruf kecil
+        //menampung angka dari -128 - 127
+        System.out.println(b);
+    }
+}

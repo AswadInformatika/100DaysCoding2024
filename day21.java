27c09516b95f84c1d3e38bd2543e88d072f3a92a
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-22 22:49:42 +0800

    Tipe Data Reference: Short

---
+++ day21.java
@@ -0,0 +1,9 @@
+package day21;
+public class Main {   
+    public static void main(String[] args) {
+    Short s = 15;
+    //tipe reference atau non primitif  
+    //tipe data short untuk bilangan bulat dari -32768 sampai 32767    
+        System.out.println(s);
+    } 
+}

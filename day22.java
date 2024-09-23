3f8fc046e521db79f3c71583de34b584f72492c7
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-23 21:59:32 +0800

    Tipe Data Reference: Integer

---
+++ day22.java
@@ -0,0 +1,10 @@
+package day22;
+public class Main {   
+    public static void main(String[] args) {
+        Integer angka = 12;
+        //tipe data non primitif atau reference
+        //yang membedakan dengan primitif adalah penulisannya
+        //kalau non primitif awal kata menggunakan huruf kapital dan tidak disingkat
+        System.out.println(angka);
+    }
+}

269bbd3b200e09650346b870adc0df9b3887a5fa
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-24 20:54:48 +0800

    Tipe Data Reference: Long

--- day23.java
+++ day23.java
@@ -1,9 +1,10 @@
-package day21;
+package day23;
 public class Main {
     public static void main(String[] args) {
-    Short s = 15;
-    //tipe data reference Long atau non primitif  
-    //tipe data Long untuk bilangan bulat bernilai tinggi
-    System.out.println(s);
-    }
+        Long l = 1000L;
+        //tipe data reference atau non primitif
+        //yang membedakan dengan primitif adalah penulisannya
+        //non primitif awal kata menggunakan huruf kapital
+        System.out.println(l);
+    } 
 }

501ef936788ae25d4f3704408f9fae70226d4f34
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-25 22:24:58 +0800

    Tipe Data Reference: Float

---
+++ day24.java
@@ -0,0 +1,9 @@
+package day24;
+public class Main {
+    public static void main(String[] args) {
+        Float f = 12.0F;
+        //tipe data reference float atau tipe data non primitif
+        //menampung bilangan desimal atau bilangan berkoma
+        System.out.println(f);
+    }
+}

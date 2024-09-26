7703fe46dc5fe39b128d066242f887a06f50e687
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-26 22:10:28 +0800

    Tipe Data Reference: Double

--- day25.java
+++ day25.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Double d = 12.0;
         //tipe data reference double atau non primitif
-        //menampung bilangan desimal atau angka berkoma
+        //tipe data double untuk menampung bilangan berkoma lebih besar dari float
         System.out.println(d);
     }
 }

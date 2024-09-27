b59426216b737cf2e97d8bd36927b8003e1f40a2
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-27 23:12:18 +0800

    Tipe Data Reference: Character

---
+++ day26.java
@@ -0,0 +1,10 @@
+package day26;
+public class Main {
+    public static void main(String[] args) {
+        //tipe data reference Character atau non primitif
+        //tipe data Character untuk 1 karakter dan dalam petik 1
+        //seperti 'a' , 'A' , '1' 
+        Character c = 'C';
+        System.out.println(c);
+    }
+}

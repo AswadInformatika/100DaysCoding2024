b39e6425a68a10e4ae3909ba64fa60bb72ce233a
AswadInformatika <aswadmajene2005@gmail.com>
2024-09-29 22:08:18 +0800

    Tipe Data Reference: Boolean

---
+++ day27.java
@@ -0,0 +1,11 @@
+package day27;
+public class Main {
+    public static void main(String[] args) {
+        Boolean belajar = true;
+        Boolean iya = false;
+        //deklarasi dan inisialisasi tipe data reference Boolean
+        //menampung nilai true dan false
+        System.out.println(belajar);
+        System.out.println(iya);
+    }
+}

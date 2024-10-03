4dd9cb814d8c85e5dac2e80228702d9256cf7fef
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-03 21:20:29 +0800

    Update day32.java

--- day32.java
+++ day32.java
@@ -5,8 +5,19 @@ public class Main {
         
         int a = 123; //konversi tipe data primitif bilangan bulat ke string
         String b = String.valueOf(a);
-
         System.out.println(b);
+        
+        byte aa = 10;
+        String bb = String.valueOf(aa);
+        System.out.println(bb);
+        
+        short cc = 100;
+        String dd = String.valueOf(cc);
+        System.out.println(dd);
+        
+        long ee = 1000;
+        String ff = String.valueOf(ee);
+        System.out.println(ff);
         
         char c = 'a'; //konversi tipe data char untuk satu karakter ke string
         String d = String.valueOf(c);
@@ -18,6 +29,10 @@ public class Main {
         
         float g = 12.0f; //konversi tipe data primitif bilangan desimal ke string
         String h = String.valueOf(g);
-        System.out.println(h);  
-    }    
+        System.out.println(h);
+        
+        double gg = 100.0;
+        String hh = String.valueOf(gg);
+        System.out.println(hh);
+    } 
 }

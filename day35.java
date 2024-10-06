146e746068b709254915b24c8d9c05cba1309d5a
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-06 22:05:03 +0800

    Operator penjumlahan dan pengurangan

---
+++ day35.java
@@ -0,0 +1,15 @@
+package day35;
+public class Main {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 2;
+      
+        //operator aritmatika penjumlahan
+        int jumlah = a + b;
+        System.out.println("Penjumlahan " + jumlah);
+        
+        //operator pengurangan
+        int kurang = a - b;
+        System.out.println("Pengurangan " + kurang);
+    }
+}

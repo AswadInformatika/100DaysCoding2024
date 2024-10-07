4aae44f7cf6a05ea78e285a63416c8558cc40b67
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-07 21:46:26 +0800

    Kali,bagi,modulus

---
+++ day36.java
@@ -0,0 +1,14 @@
+package day36;
+public class Main {  
+    public static void main(String[] args){
+       int a = 10;
+       int b = 3;
+       //operator aritmatika
+       int kali = a * b; 
+       System.out.println("Perkalian " + kali);
+       int bagi = a / b;
+       System.out.println("Pembagian " + bagi);
+       int modulus = a % b;
+       System.out.println("Modulus " + modulus);
+   }    
+}

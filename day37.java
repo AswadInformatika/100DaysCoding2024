e46e0381d6168b9059e0245c0080c7ea3c94e1e8
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-08 22:26:04 +0800

    Penugasan: pengisian nilai, penambahan, pengurangan

---
+++ day37.java
@@ -0,0 +1,18 @@
+package day37;
+public class Main {
+    public static void main(String[] args) {
+        //operator penugasan
+        
+        //pengisian nilai
+        int a = 10;
+        System.out.println("Nilai " + a);
+        
+        //penambahan
+        a += 3;
+        System.out.println("Hasil penambahan " + a);
+        
+        //pengurangan
+        a -= 5;
+        System.out.println("Hasil pengurangan " + a);
+    }
+}

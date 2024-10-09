cad35390d95beff66dc4c9e7337a6c2428ea106c
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-09 23:22:37 +0800

    Operator penugasan: perkalian dan pembagian

---
+++ day38.java
@@ -0,0 +1,10 @@
+package day38;
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        a *= 2; //operator penugasa  perkalian
+        System.out.println("Perkalian " + a);
+        a /= 2; //operator penugasan pembagian
+        System.out.println("Pembagian " + a);
+    }  
+}

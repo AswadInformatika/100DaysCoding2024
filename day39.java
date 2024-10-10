0caafd74de22db790d49a96fa8cb00b6cb835bf4
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-10 22:28:17 +0800

    Operator penugasan: pengisian dan modulo

---
+++ day39.java
@@ -0,0 +1,8 @@
+package day39;
+public class Main {
+    public static void main(String[] args) {
+        int modulo = 10; //operator penugasan pengisian
+        modulo %=3; //operator penugasan modulo
+        System.out.println("Sisa bagi: " + modulo);
+    }
+}

b3292cc4586ea1e35021641ba6de4865b9720bd4
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-11 22:13:25 +0800

    Operator perbandingan: lebih besar dan lebih kecil

---
+++ day40.java
@@ -0,0 +1,12 @@
+package day40;
+public class Main {
+    public static void main(String[] args) {
+        //operator perbandingan
+        int a = 5; 
+        int b = 3; 
+        boolean besar = a > b; //lebih besar
+        boolean kecil = a < b; //lebih kecil
+        System.out.println("a lebih besar dari b? " + besar);
+        System.out.println("a lebih kecil dari b? " + kecil);
+    }
+}

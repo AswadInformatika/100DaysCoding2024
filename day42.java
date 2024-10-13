c614b054de7e33258a7338636148def67902e864
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-13 18:38:13 +0800

    Operator penugasan: sama dengan dan tidak sama dengan

---
+++ day42.java
@@ -0,0 +1,12 @@
+package day42;
+public class Main {
+    public static void main(String[] args) {
+        //operator penugasan
+        int a = 5;
+        int b = 2;
+        boolean samaDengan = a == b; //sama dengan
+        System.out.println(samaDengan);
+        boolean tidakSamaDengan = a != b; //tidak sama dengan
+        System.out.println(tidakSamaDengan);
+    }
+}

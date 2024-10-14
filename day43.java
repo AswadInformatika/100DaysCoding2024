e63f837df735028a0e9748bf450b769273efce6e
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-14 21:54:01 +0800

    Operator perbandingan: lebih besar sama dengan dan lebih kecil sama dengan

---
+++ day43.java
@@ -0,0 +1,11 @@
+package day43;
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+        boolean c = a>=b; //operator perbandingan lebih besar sama dengan
+        System.out.println(c);
+        boolean d = a<=b; //operator perbandingan lebih kecik sama dengan
+        System.out.println(d);
+    }
+}

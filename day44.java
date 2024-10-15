92a4e2d5741a840fcce3e91050e81962bf4b699e
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-15 21:34:26 +0800

    Operator logika AND

---
+++ day44.java
@@ -0,0 +1,10 @@
+package day44;
+public class Main {
+    public static void main(String[] args) {
+        //operator logika AND
+        //akan bernilai true jika nilai di kiri dan kanannya bernilai true, sisanya false
+        int a = 2;
+        int b = 3;
+        System.out.println(a > b && b < a);
+    }
+}

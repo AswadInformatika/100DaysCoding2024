7e74cc9e8bda62a5e27a42f7fe2a50a90b8142c8
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-16 20:15:22 +0800

    Operator logika OR

---
+++ day45.java
@@ -0,0 +1,11 @@
+package day45;
+public class Main {
+    public static void main(String[] args) {
+        //operator logika OR
+        //operator logika or hanya akan menampilkan false apabila kedua nilai sebelah kiri atau kanannya semuanya false, sisanya true
+        int pertama = 4;
+        int kedua = 5;
+        System.out.println(pertama > kedua || kedua > pertama);
+    }
+    
+}

244db8f746681339c7af36513a5853ee0a9ffd8e
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-17 21:58:36 +0800

    Operator logika NOT

---
+++ day46.java
@@ -0,0 +1,9 @@
+package day46;
+public class Main {
+    public static void main(String[] args) {
+        //operator logika not! di gunakan untuk membalikkan nilai boolean
+        //jika nilai true di balikkan akan menghasilkan false, sebaliknya
+        boolean ya = true;
+        System.out.println(!ya);
+    }
+}

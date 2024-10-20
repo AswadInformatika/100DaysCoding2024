5b4b80c067dc92e75ad589473b13f5502b106578
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-20 21:36:07 +0800

    Percabangan if, else if dan else

--- day49.java
+++ day49.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
       Scanner isc = new Scanner(System.in);
         System.out.print("Masukkan angka pertama: ");
-        int angkaPertama = isc.nextInt();
+        int a = isc.nextInt();
         System.out.print("Masukkan angka kedua: ");
-        int angkaKedua = isc.nextInt();
+        int b = isc.nextInt();
         System.out.print("Masukkan angka ketiga: ");
-        int angkaKetiga = isc.nextInt();
-        if(angkaPertama > angkaKedua && angkaPertama > angkaKetiga){
+        int c = isc.nextInt();
+        if(a > b && a > c){
             System.out.println("Angka terbesar adalah " + angkaPertama);
-        }else if(angkaKedua > angkaPertama && angkaKedua > angkaKetiga){
+        }else if(b > a && b > c){
             System.out.println("Angka terbesar adalah " + angkaKedua);
         }else{
             System.out.println("Angka terbesar adalah " + angkaKetiga);

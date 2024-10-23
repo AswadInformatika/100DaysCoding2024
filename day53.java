a2bb6cb4082847ad4e53fb619861f063ad7979f0
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-24 00:02:04 +0800

    Ternary

--- day53.java
+++ day53.java
@@ -1,10 +1,7 @@
-public class TernaryExample {
+public class Main {
     public static void main(String[] args) {
         int angka = 10;
-
-        // Ternary operator untuk mengecek apakah angka genap atau ganjil
         String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
-
         System.out.println("Angka " + angka + " adalah " + hasil);
     }
 }

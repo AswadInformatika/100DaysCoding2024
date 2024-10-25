6539aaad15e7ad38696a877ec73f11eca2af8bda
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-25 19:46:38 +0800

    Perulangan for increment

--- day54.java
+++ day54.java
@@ -2,7 +2,11 @@ package day54;
 public class Main {
     public static void main(String[] args) {
         for(int i = 1; i <= 10; i++){
-           System.out.println(i); 
+            if(i % 2 == 0){
+                System.out.println(i + " genap");
+            }else{
+                System.out.println(i + " ganjil");
+            }               
         }
     }
 }

aed7c8416f1e3176f3c1d5cce7c34441f48368f0
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-26 17:14:27 +0800

    Perulangan for decrement

---
+++ day55.java
@@ -0,0 +1,8 @@
+package day55;
+public class Main {
+    public static void main(String[] args) {
+        for(int a = 100; a >= 0 ; a-=5){
+           System.out.println(a); 
+        }
+    }  
+}

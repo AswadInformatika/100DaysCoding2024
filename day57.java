f5be38ca4c173d783a05773032b88069a12a9181
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-28 21:20:17 +0800

    Perulangan while increment

---
+++ day57.java
@@ -0,0 +1,10 @@
+package day57;
+public class Main {
+    public static void main(String[] args) {
+        int i = 2;
+        while(i <= 20){
+        System.out.println(i);
+        i+=2;
+        }
+    }
+}

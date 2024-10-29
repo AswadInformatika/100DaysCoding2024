f229dacbe4c961c41ffe35ec0d2687516ee857ba
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-29 19:36:36 +0800

    Perulangan while decrement

---
+++ day58.java
@@ -0,0 +1,10 @@
+package day58;
+public class Main {
+    public static void main(String[] args) {
+        int i = 20;
+        while (i >= 2) {
+        System.out.print(i+ " ");
+        i -= 2;
+       }
+    }
+}

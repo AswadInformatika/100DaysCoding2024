fbab3391e28e113005ad817a0cf66b93e462c52e
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-30 22:57:48 +0800

    Perulangan for each

---
+++ day59.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        System.out.println("Total: " + sum);
+    }
+}

47b4e32856bb8fe92ed17774e3b7020390ff4bfe
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-30 23:07:46 +0800

    Perulangan for-each

--- day59.java
+++ day59.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        int sum = 0;
-        for (int number : numbers) {
-            sum += number;
+        int[] n = {1, 2, 3, 4, 5};
+        int total = 0;
+        for (int number : n) {
+            total += number;
         }
-        System.out.println("Total: " + sum);
+        System.out.println("Total: " + total);
     }
 }

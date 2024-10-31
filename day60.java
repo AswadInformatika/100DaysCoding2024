da2a869f86efacad8c7d6de33becee16bd562639
AswadInformatika <aswadmajene2005@gmail.com>
2024-10-31 21:14:39 +0800

    Perulangan do while

---
+++ day60.java
@@ -0,0 +1,10 @@
+package day60;
+public class Main {
+    public static void main(String[] args) {
+        int n = 3;
+        do {
+            System.out.println(n);
+            n += 3;
+        } while (n <= 15);
+    }
+}

29f011ec512fc78e7fa73e9eac5269789257e6e3
AswadInformatika <aswadmajene2005@gmail.com>
2024-11-04 21:35:00 +0800

    Break label

--- day64.java
+++ day64.java
@@ -1,13 +1,13 @@
 package day64;
 public class Main {
     public static void main(String[] args) {
-        oneLoop:
+        swad:
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
             
             if (i == 3) {
                 System.out.println("Break pada i = " + i);
-                break oneLoop;
+                break swad;
             }
         }
         System.out.println("Loop selesai.");

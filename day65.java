854c1616f8732ddd7a5049a5c7e92886b9457e0c
AswadInformatika <aswadmajene2005@gmail.com>
2024-11-05 22:15:47 +0800

    Nested loop

---
+++ day65.java
@@ -0,0 +1,12 @@
+package day65;
+public class Main {
+    public static void main(String[] args) {
+        int y = 3;
+        for(int a = 1; a <= y; a++){
+            for(int b = 1; b <= y; b++){
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+    }
+}

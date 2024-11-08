bf0dc6e7e36d7de34d1b7eb404db2da9e10f1468
AswadInformatika <aswadmajene2005@gmail.com>
2024-11-08 21:19:30 +0800

    Method non void

---
+++ day68.java
@@ -0,0 +1,9 @@
+public class NonVoid {
+    public static void main(String[] args) {
+       NonVoid swad = new NonVoid();
+        System.out.println(swad.angka());
+    }
+     int angka(){
+        return 19;
+     }
+}

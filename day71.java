b10e62f573fabbc50cce69f1fa91b8a4d011bb31
AswadInformatika <aswadmajene2005@gmail.com>
2024-11-11 23:43:37 +0800

    Method static

---
+++ day71.java
@@ -0,0 +1,16 @@
+package day71;
+
+class aritmatika{
+public static int perkalian(int a, int b) {
+    return a*b;
+  }
+}
+
+public class Main {
+public static void main(String[] args) {
+int x = 2;
+int y = 10;
+int S = aritmatika.perkalian (x, y);
+System.out.println(S);
+     }
+}

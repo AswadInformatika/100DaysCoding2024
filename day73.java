c190c42817aab9ddec8e03a2aba038207dba57d9
AswadInformatika <aswadmajene2005@gmail.com>
2024-11-13 22:23:34 +0800

    String Method : charAt()

---
+++ day73.java
@@ -0,0 +1,17 @@
+package day73;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nama: ");
+        String nama = sc.nextLine();
+
+        System.out.print("Masukkan indeks: ");
+        int indeks = sc.nextInt();
+
+        char karakter = nama.charAt(indeks);
+        System.out.println("Indeks ke " + indeks + " adalah huruf: " + karakter);
+        
+    }
+}

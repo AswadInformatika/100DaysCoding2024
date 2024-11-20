7377d6da0113dc6e5aaefeb9365ccca36d91440e
AswadInformatika <aswadmajene2005@gmail.com>
2024-11-20 21:15:12 +0800

    String method : trim()

---
+++ day80.java
@@ -0,0 +1,28 @@
+package day80;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String nama;
+        while (true) {
+            System.out.print("Masukkan nama (tidak boleh kosong): ");
+            nama = scanner.nextLine().trim();
+            if (nama.length() > 0) {
+                break;
+            }
+            System.out.println("Nama tidak boleh kosong. Silakan coba lagi.");
+        }
+
+        cekNama(nama);
+    }
+
+    public static void cekNama(String nama) {
+        if (nama.length() <= 5) {
+            System.out.println(nama +" nama yang singkat");
+        } else {
+            System.out.println(nama + " nama yang panjang");
+        }
+    }
+}

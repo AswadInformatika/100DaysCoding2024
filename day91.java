fef5266fa63f1de6bed2cfbc361f55108076df74
AswadInformatika <aswadmajene2005@gmail.com>
2024-12-01 21:11:45 +0800

    Soal

--- day91.java
+++ day91.java
@@ -1,7 +1,7 @@
 package day;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         while (true) {

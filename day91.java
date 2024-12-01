4ff3b1cf3ddf6134d6ae605b03ddb9ff442c4a73
AswadInformatika <aswadmajene2005@gmail.com>
2024-12-01 21:04:18 +0800

    Soal

---
+++ day91.java
@@ -0,0 +1,18 @@
+package day;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Msukkan angka: ");
+            int input = sc.nextInt();
+            if (input % 2 == 0) {
+                System.out.println(input + ": angka genap");
+            }else if (input % 2 == 1) {
+                System.out.println("You and I, END");
+                break;
+            }
+        }
+    }
+}

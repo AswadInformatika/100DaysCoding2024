718f91036c4ead0e6f4e51178e9c6fae071ce269
AswadInformatika <aswadmajene2005@gmail.com>
2024-12-02 22:09:15 +0800

    Pola V

---
+++ day92.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            for (int j = n - 1; j >= 0; j--) {
+                if (j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

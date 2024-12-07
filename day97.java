5614158ff45a96b34b0ee64c65b8248a6fcc4296
AswadInformatika <aswadmajene2005@gmail.com>
2024-12-07 23:11:09 +0800

    Bilangan prima

--- day97.java
+++ day97.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan bilangan: ");
         int n = sc.nextInt();
-        boolean p = true;
+        
+        System.out.println("Bilangan prima dari 1 hingga " + n + ":");
 
-        if (n < 2) {
-            p = false;
-        } else {
-            for (int i = 2; i <= n / 2; i++) {
-                if (n % i == 0) {
+        for (int m = 2; m <= n; m++) {
+            boolean p = true;
+
+            for (int i = 2; i <= m / 2; i++) {
+                if (m % i == 0) {
                     p = false;
                     break;
                 }
             }
-        }
 
-        if (p) {
-            System.out.println(n + " adalah bilangan prima");
-        } else {
-            System.out.println(n + " bukan bilangan prima");
+            if (p) {
+                System.out.print(m + " ");
+            }
         }
     }
 }
